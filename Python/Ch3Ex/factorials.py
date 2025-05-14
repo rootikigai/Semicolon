1fac7cb12a345424b28b7547ae715fff216a2f44
rootikigai <ifykel360@gmail.com>
2025-05-14 21:44:27 +0100

    Finding factorials of numbers: written with loops

---
+++ Python/Ch3Ex/factorials.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a non-negative integer: "))
+
+factorial = 1
+
+for i in range(1, number + 1):
+    factorial = factorial * i
+
+print(f"The factorial of {number} is {factorial}")
+
