9f3377d91c17aa3df97989426d03c145aae28f30
rootikigai <ifykel360@gmail.com>
2025-05-14 21:41:51 +0100

    String knowledge test

--- Python/Ch3Ex/TuringTest.py
+++ Python/Ch3Ex/TuringTest.py
@@ -1,7 +1,9 @@
 print('What is your problem?: ')
-response = input('Have you had this problem before(yes or no)?: ')
+response = input('Have you had this problem before(yes or no)?: ').lower()
 if response == 'yes':
     print('Well, you have it again')
 elif response == 'no':
     print('Well, you have it now.')
+else:
+    print('Response is invalid.')
 
