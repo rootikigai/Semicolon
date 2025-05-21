0159d2567dab75e3bf766eac2c01005901ede26e
rootikigai <ifykel360@gmail.com>
2025-05-21 20:28:58 +0100

    Testing ternary operator in selection statement

---
+++ JavaScript/selectionanditeration.js
@@ -0,0 +1,34 @@
+/*
+const prompt = require("prompt-sync")();
+let score = prompt("What is your score?: ");
+console.log(score + " is your score.");
+--
+let score = 50
+let div = Math.floor(score/10);
+switch(div){
+    case 1:
+    case 2:
+    case 3:
+    case 4: console.log("P")
+    break
+    case 5: console.log("P")
+    break
+    case 6: console.log("C")
+    break
+    case 7: console.log("B")
+    break
+    case 8: console.log("B")
+    break
+    case 9: console.log("A")
+    break
+    case 10: console.log("A")
+    default: console.log("NaN")
+    break
+
+//console.log(score)
+}
+*/
+
+//Ternary Operators
+let scores = 49;
+(scores < 50)? console.log("F") : console.log("A")
