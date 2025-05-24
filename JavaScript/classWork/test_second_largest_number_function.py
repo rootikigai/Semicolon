f08f7de392bc6d8d989a0b1a38c1ab150567a646
rootikigai <ifykel360@gmail.com>
2025-05-24 21:03:28 +0100

    Testing the fuction to return the second largest number ina list

---
+++ JavaScript/classWork/test_second_largest_number_function.py
@@ -0,0 +1,7 @@
+import second_largest_number_function
+from unittest import TestCase
+
+class test_second_largest_number_function(TestCase):
+
+    def test_that_second_largest_function_exists(self):
+        second_largest_number_function.second_largest([0, 0])
