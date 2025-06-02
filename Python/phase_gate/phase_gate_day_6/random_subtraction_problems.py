78c34e6df16c6293b263f6f329d2eb8dc83b7258
rootikigai <ifykel360@gmail.com>
2025-06-02 16:53:30 +0100

    Wrote a program that generates random math subtraction questions for a user and gives the total of wrong and correct answers.

---
+++ Python/phase_gate/phase_gate_day_6/random_subtraction_problems.py
@@ -0,0 +1,33 @@
+programRun = 0
+numOfTrials = 0
+countCorrect = 0
+countWrong = 0
+wrong = 0
+ans = 0
+while(programRun != 10):
+    num1 == int(Math.floor(Math.random() * 100))
+    num2 == int(Math.floor(Math.random() * 100))
+    if(programRun > 9):
+        break
+    else:
+        if(num1 < num2):
+            temp = num1
+            num1 = num2
+            num2 = temp
+        
+        result = num1 - num2
+        ans = print(input("What is " + num1 + " - " + num2 + "?: "))
+        if(ans == result):
+            print(ans + " is correct.")
+            countCorrect = countCorrect + 1
+        elif(ans != result):
+            for wrong in range(0, 1, +1):
+                print("Wrong. One trial left!")
+                ans = print(input("What is " + num1 + " - " + num2 + "?:"))
+
+            countWrong = countWrong + 1;
+        
+    print(countCorrect + " correct score!");
+    print(countWrong + " wrong score!");
+    programRun = programRun + 1;
+
