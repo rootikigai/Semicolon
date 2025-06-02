907e616511ce6c423b585daf5715fd89a958f557
rootikigai <ifykel360@gmail.com>
2025-06-02 16:50:06 +0100

    Wrote a program that generates random subtraction questions and calculates scores at the end...mods completed!

--- JavaScript/phaseGateDay6/RandomSubtractionProblems.js
+++ JavaScript/phaseGateDay6/RandomSubtractionProblems.js
@@ -1,31 +1,40 @@
-let num1 = Math.floor(Math.random() * 100);
-let num2 = Math.floor(Math.random() * 100);
+let programRun = 0;
 let numOfTrials = 0;
 let countCorrect = 0;
 let countWrong = 0;
 let wrong = 0;
 let ans = 0;
-    if(num1 < num2){
-        let temp = num1;
-        num1 = num2;
-        num2 = temp;
+while(programRun != 10){
+    let num1 = Math.floor(Math.random() * 100);
+    let num2 = Math.floor(Math.random() * 100);
+    if(programRun > 9){
+        break;
     }
-    let result = num1 - num2;
-for(let trials = 0; trials <= 9; trials++){
-    const prompt = require("prompt-sync")();
-    let ans = prompt("What is " + num1 + " - " + num2 + "?: ");
-        if(ans == result){
-            console.log(ans + " is correct.");
-            countCorrect = countCorrect + 1;
+    else{
+        if(num1 < num2){
+            let temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
-        else if(ans != result){
-            for(wrong = 0; wrong < 1; wrong++){
-                console.log("Wrong. One trial left!");
-                const prompt = require("prompt-sync")();
-                let ans = prompt("What is " + num1 + " - " + num2 + "?:");
+        let result = num1 - num2;
+  //  for(let trials = 0; trials <= 9; trials++){
+        const prompt = require("prompt-sync")();
+        let ans = prompt("What is " + num1 + " - " + num2 + "?: ");
+            if(ans == result){
+                console.log(ans + " is correct.");
+                countCorrect = countCorrect + 1;
             }
-            countWrong = countWrong + 1;
-        }
-    console.log(countCorrect + " correct score!");
-    console.log(countWrong + " wrong score!");
+            else if(ans != result){
+                for(wrong = 0; wrong < 1; wrong++){
+                    console.log("Wrong. One trial left!");
+                    const prompt = require("prompt-sync")();
+                    let ans = prompt("What is " + num1 + " - " + num2 + "?:");
+                }
+                countWrong = countWrong + 1;
+            }
+        console.log(countCorrect + " correct score!");
+        console.log(countWrong + " wrong score!");
+        programRun = programRun + 1;
+    }
 }
+
