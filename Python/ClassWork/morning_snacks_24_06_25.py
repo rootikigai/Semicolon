72fdf0d0a8335125620ab46929842c14eaea3da2
rootikigai <ifykel360@gmail.com>
2025-06-24 16:51:07 +0100

    Writing simple functions for lists: Changing the value of the second index in a list

--- Python/ClassWork/morning_snacks_24_06_25.py
+++ Python/ClassWork/morning_snacks_24_06_25.py
@@ -3,3 +3,13 @@ def get_third_elem(numbers):
 
 get_third_elem([5, 3, 22, 6, 70, 42])
 print('---------------------------')
+
+
+def change_second_index(things, changer):
+    things = list(things)
+    things[2] = changer
+    return things
+
+print(change_second_index(["red", "yellow", "blue", "green", "black"], "white"))
+
+print('----------------------------')
