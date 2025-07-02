0d565e95c248e27f79f65849c1d907ff6579e9d7
rootikigai <ifykel360@gmail.com>
2025-07-02 19:50:48 +0100

    Function that returns valid priority numbers from an array of valid and invalid priority numbers

---
+++ JavaScript/classWork/removeBugs.js
@@ -0,0 +1,9 @@
+
+function removeBugs(priorityNums){
+    priorityNums = []
+    for(let num = 1; num <= 5; num++){
+        priorityNums.push(num)        
+    }
+    return priorityNums
+}
+console.log(removeBugs(0,1,2,3,4,5,6))
