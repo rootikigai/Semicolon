06a9ebb638aa9db940f1230898514fbe9793c1cb
rootikigai <ifykel360@gmail.com>
2025-07-06 14:29:07 +0100

    Cleaned up functions for rating movies after Tests done. Now Ratings append to a list for easy average_ratings calculation

--- Python/phase_gate/movie_rating_system/movie_rating_functions.py
+++ Python/phase_gate/movie_rating_system/movie_rating_functions.py
@@ -1,42 +1,38 @@
 from datetime import datetime
 movie_folder = []
-movie_rating_system = []
+date = datetime.now().strftime("%B %d, %Y, %I:%M %p")
 
 def current_date_and_time():
-    return datetime.now().strftime("%B %d, %Y, %I:%M %p")
+    return date
 
 def see_movie_folder():
     return movie_folder
 
-def add_movie(movie_title):
-    now = datetime.now()
-    formatted_date = now.strftime("%B %d, %Y, %I:%M %p")
-    movie_folder.append([movie_title, formatted_date])
+def add_movie(movie_name):
+    movie = {"Title": movie_name, "Date": date, "Movie Ratings": []}
+    movie_folder.append(movie)
+    return f"Movie - {movie_name}, was added on {movie['Date']}"
 
-    return [movie_title, formatted_date]
 
-def rate_movie(movie_title, rating):
+def rate_movie(movie_name, rating):
     rating = int(rating)
     if 0 <= rating <= 5:
-        rated = [add_movie(movie_title), rating]
-        movie_folder.extend(rated)
-    return "Movie - {}, was rated: {}".format(movie_title, rating)
-
-def find_movie(movie_title):
-    for movie in movie_folder:
-        if movie[0] == movie_title:
-            return movie[1]
+        for movie in movie_folder:
+            if movie['Title'] == movie_name:
+                movie['Movie Ratings'].append(rating)
+                return f"Movie - {movie_name}, was rated: {rating}"
     return None
 
 
-#print(add_movie("Koto Aye"))
+print(add_movie("Koto Aye"))
 #print(add_movie("Ipadabo Abija"))
 #print(add_movie("Glamor Girls"))
-#print()
+print()
 print(rate_movie("Koto Aye", 5))
 print(rate_movie("Koto Aye", 4))
+
 print()
-#print(see_movie_folder())
-#print()
-print(find_movie("Koto Aye"))
+print(see_movie_folder())
+print()
+#print(find_movie("Koto Aye"))
 #print(current_date_and_time())
--- Python/phase_gate/movie_rating_system/movie_rating_test.py
+++ Python/phase_gate/movie_rating_system/movie_rating_test.py
@@ -1,23 +1,23 @@
 import unittest
 from unittest import TestCase
-from datetime import datetime
 
-from movie_rating_system.movie_rating_functions import movie_folder, add_movie, see_movie_folder, current_date_and_time, \
-    rate_movie
+from movie_rating_system.movie_rating_functions import *
 
 
 class MovieRatingTest(TestCase):
 
-    def test_that_movie_folder_is_empty(self):
-        self.assertTrue(len(movie_folder) == 0)
+    def test_that_movie_folder_is_not_empty(self):
+        add_movie("Koto Aye")
+        self.assertTrue(len(movie_folder) > 0)
 
     def test_that_movie_can_be_added(self):
-        add_movie("Koto Aye")
-        self.assertEqual(see_movie_folder(), [["Koto Aye", current_date_and_time()]])
+        self.assertEqual(add_movie("Koto Aye"), f"Movie - Koto Aye, was added on {current_date_and_time()}")
 
     def test_that_movie_can_be_rated(self):
-        rate_movie("Koto Aye", 5)
-        self.assertEqual(rate_movie(movie_title, rating), "Movie - {}, was rated: {}".format(movie_title, rating))
+        self.assertEqual(rate_movie(5), "Movie - Koto Aye, was rated: 5")
+
+    def test_get_average_rating(self):
+        self.assertEqual((get_average_rating("Koto Aye")), "Average rating for Koto Aye is 5")
 
 if __name__ == '__main__':
     unittest.main()
