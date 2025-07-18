f301e8eaf14c702ea4332e3600201668e005565a
rootikigai <ifykel360@gmail.com>
2025-07-18 21:52:43 +0100

    test updated to include test to check that function returns second argument appended to first string argument

--- Python/ClassWork/strings_play_test.py
+++ Python/ClassWork/strings_play_test.py
@@ -6,3 +6,7 @@ class TestStringsPlay(unittest.TestCase):
     def test_function_returns_second_parameter_in_between_string(self):
         play = strings_play_main
         self.assertEqual("helceloo", play.strings_play("helloo", param2="ce"))
+
+    def test_function_returns_second_parameter_appended_to_string(self):
+        play = strings_play_main
+        self.assertEqual("joyce", play.strings_play("joy", param2="ce"))
