680335a3f8ef49edf7f3bf5bee94e9128bdba5af
rootikigai <ifykel360@gmail.com>
2025-07-19 11:51:02 +0100

    Code updated to make second test pass

--- Python/ClassWork/strings_play_main.py
+++ Python/ClassWork/strings_play_main.py
@@ -1,8 +1,12 @@
 def strings_play(string1 = "", param2 = "ce"):
-    mid = len(string1) // 2
-    half1 = string1[:mid]
-    half2 = string1[mid:]
-    new_word = half1 + param2 + half2
-    return new_word
+    if len(string1) % 2 != 0:
+        string1 = string1 + param2
+        return string1
+    else:
+        mid = len(string1) // 2
+        half1 = string1[:mid]
+        half2 = string1[mid:]
+        new_word = half1 + param2 + half2
+        return new_word
 
-print(strings_play("helloo", param2="ce"))
+print(strings_play("joy", param2="ce"))
