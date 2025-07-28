3e59352af432e7a9287460e892b550b24ff44dcf
rootikigai <ifykel360@gmail.com>
2025-07-28 23:14:38 +0100

    TDD and Java Functions Practice

---
+++ JAVA/Mr-Chi/TestDrillerCBTDriver.java
@@ -0,0 +1,7 @@
+public class TestDrillerCBTDriver{
+    public static void main(String[] args){
+        int copies = 0;
+        int priceOfCopies = TestDrillerCBT.getCBTPrice(copies);
+        System.out.println(priceOfCopies);
+    }
+}
