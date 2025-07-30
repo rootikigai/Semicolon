d99bba831ad67456415a97e6a675d31d36c15f98
rootikigai <ifykel360@gmail.com>
2025-07-30 19:03:37 +0100

    Task done

---
+++ JAVA/JavaPersonal/OneFive.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class OneFive{
+	public static void main(String[] args){
+  int sum = 0;
+  int num;
+    for(num = 10; num >= 1; num--){
+      sum += num; 
+		  System.out.println(num); }
+		  System.out.println(sum + " Is the sum of 10 natural number.");
+System.out.println("--------------------------------------");
+while true:
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = input.nextInt();
+    if(number % 2 == 0){
+      System.out.println(number + " is an even number.");
+    }
+    else{
+      System.out.println(number + " is an odd number.");
+    }
+	}
+}
