22905882c888d07ee2daf15d02384bd5eff54e97
rootikigai <ifykel360@gmail.com>
2025-08-02 23:03:30 +0100

    Task today

---
+++ Tutorials/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator{
+	public static int subtract(int num1, int num2){
+		return num1 - num2;
+	}
+  public static int multiply(int num1, int num2){
+    return num1 * num2;
+  }
+  public static int divide(int num1, int num2){
+    return num1 / num2;
+  }
+}
