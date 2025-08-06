f046c221c6522b67e77f50ba27a0cc13b12b1c19
rootikigai <ifykel360@gmail.com>
2025-08-06 23:32:11 +0100

    Today's task 2

---
+++ Python/Ch4Ex/rounding_numbers.py
@@ -0,0 +1,17 @@
+num = 13.56449
+
+nearest_integer = round(num)
+
+nearest_tenth = round(num, 1)
+
+nearest_hundredth = round(num, 2)
+
+nearest_thousandth = round(num, 3)
+
+nearest_ten_thousandth = round(num, 4)
+
+print("Nearest Integer:", nearest_integer)
+print("Nearest Tenth:", nearest_tenth)
+print("Nearest Hundredth:", nearest_hundredth)
+print("Nearest Thousandth:", nearest_thousandth)
+print("Nearest Ten Thousandth:", nearest_ten_thousandth)
