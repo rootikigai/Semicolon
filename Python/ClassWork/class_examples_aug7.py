37398e3394e135fc6669b583c224ccf4df2f1b8b
rootikigai <ifykel360@gmail.com>
2025-08-07 23:24:20 +0100

    Testing out OOP Principle of Inheritance in Python...Employee class added and tying it all up

--- Python/ClassWork/class_examples_aug7.py
+++ Python/ClassWork/class_examples_aug7.py
@@ -7,3 +7,14 @@ class Human:
     def __str__(self):
         return f'Name of Employee {self.name}\nDate of Birth: {self.dob}'
 
+class Employee(Human):
+    def __init__(self, name, dob, employee_id):
+        super().__init__(name, dob)
+        self.employee_id = employee_id
+
+    def __str__(self):
+        return f'{super().__str__()}\nEmployee ID: {self.employee_id}'
+
+h1 = Human('Khalid', '1990-01-01')
+e1 = Employee("Kelvin", "1990-01-01", "001")
+print(h1, e1)
