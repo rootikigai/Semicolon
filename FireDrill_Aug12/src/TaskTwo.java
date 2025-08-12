976b35bc1ae4b586ba40339b34e0de4ced118bf6
rootikigai <ifykel360@gmail.com>
2025-08-12 10:14:56 +0100

    FireDrill TaskTwo

---
+++ FireDrill_Aug12/src/TaskTwo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        int average = 0;
+        for (int index = 0; index < 10; index++) {
+            System.out.print("Enter number " + (index + 1) + ": ");
+            sum += input.nextInt();
+            count++;
+        }
+        average = sum / count;
+        System.out.println("The average is " + average);
+    }
+}
