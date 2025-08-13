ef534ee2b88fc0ad7003901e43fd3b4b6fc5f811
rootikigai <ifykel360@gmail.com>
2025-08-13 19:42:00 +0100

    FireDrill TaskFour corrected.

--- FireDrill_Aug12/src/TaskFour.java
+++ FireDrill_Aug12/src/TaskFour.java
@@ -4,12 +4,15 @@ public class TaskFour {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int sum = 0;
+        int count = 0;
         for (int index = 0; index < 10; index++) {
             System.out.print("Enter number " + (index + 1) + ": ");
-            if (index % 2 != 0){
-                sum += input.nextInt();
+            int score = input.nextInt();
+            count++;
+            if(count % 2 == 0) {
+                sum += score;
             }
         }
-        System.out.println("The sum of even is " + sum);
+        System.out.println("The sum of even indexes is " + sum);
     }
 }
