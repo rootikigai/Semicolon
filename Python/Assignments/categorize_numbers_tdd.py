e9e7a50b59d0e76c1d9614fa509b1c2b60dff9ba
rootikigai <ifykel360@gmail.com>
2025-09-10 23:27:41 +0100

    Practice Assignment 2

---
+++ Python/Assignments/categorize_numbers_tdd.py
@@ -0,0 +1,7 @@
+import categorize_numbers
+from unittest import TestCase
+
+class test_categorize_numbers(TestCase):
+
+    def test_that_categorize_numbers_exists(self):
+        categorize_numbers.categorize_numbers()
