ce2d9fd786ea4424f54ae382f3f3c956591de21e
rootikigai <ifykel360@gmail.com>
2025-09-11 23:32:14 +0100

    Today's task 1 test

--- Python/Assignments/bank_test.py
+++ Python/Assignments/bank_test.py
@@ -5,5 +5,9 @@ class BankTest(TestCase):
     def test_that_create_account_function_exists(self):
         account = bank.create_account("Kelvin Ikigai")
         self.assertEqual(account["name"], "Kelvin Ikigai")
-        self.assertEqual(account[balance], 0.0)
+        self.assertEqual(account["balance"], 0.0)
 
+    def test_that_deposit_function_works(self):
+        account = bank.create_account("Ying Yang")
+        bank.deposit_funds(account, 200000)
+        self.assertEqual(account["balance"], 200000)
