e5a9511676af361fd7143fdcca85ea78d7fe5230
rootikigai <ifykel360@gmail.com>
2025-09-11 23:31:17 +0100

    Today's task 1

--- Python/Assignments/bank.py
+++ Python/Assignments/bank.py
@@ -1,4 +1,11 @@
 def create_account(name):
-    return{"name": name, "balance": 0.0}
+    return{"name": name, "balance": 0.00}
 
 #print(create_account("Kelvin Ikigai"))
+
+def deposit_funds(account, amount):
+    account["balance"] += amount
+
+account = create_account("Ying Yang")
+deposit_funds(account, account["balance"])
+#print(f"Your new account is created and deposit has been made into your account, {account}")
