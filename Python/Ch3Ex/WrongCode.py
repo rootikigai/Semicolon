8f0e9a915e6ed91539e4f902a4647cd01bb76bdf
rootikigai <ifykel360@gmail.com>
2025-09-11 23:32:44 +0100

    Today's task 2

--- Python/Ch3Ex/WrongCode.py
+++ Python/Ch3Ex/WrongCode.py
@@ -1,4 +1,7 @@
 a = b = 7
 print('a =', a, '\nb =', b)
 
+#It gives an output of 
+# a = 7
+# b = 7
 #Nothing is wrong with the code as far as running it is concerned. But, the convention of initializing variables may have been overlooked here.
