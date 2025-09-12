624c240059c5d69a29955c56b33d713710a2ecfb
rootikigai <ifykel360@gmail.com>
2025-09-12 23:40:12 +0100

    Arrays task

--- JAVA/JavaPersonal/ArrayTest.java
+++ JAVA/JavaPersonal/ArrayTest.java
@@ -5,8 +5,8 @@ public class ArrayTest{
     double[] myList = new double[10];
     java.util.Scanner input = new java.util.Scanner(System.in);
     System.out.print("Enter " + myList.length + " values: ");
-    for (int i = 0; i < myList.length; i++)
-    myList[i] = input.nextDouble()
+    for (int i = 0; i < myList.length; i++);
+    myList[i] = input.nextDouble();
     System.out.print(myList[i] + " ");
   }
 }
